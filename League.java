6725514870b0ead6092e438651ef37d5cadb8e49
R Murphy <rachel.lee.murphy@gmail.com>
2018-09-19 19:08:50 -0700

    Add files via upload
    This is a practice push for A1 SENG330

---
+++ League.java
@@ -0,0 +1,16 @@
+package ca.uvic.seng330.assn1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class League implements Iterable<Team> {
+
+	@Override
+	public Iterator<Team> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
